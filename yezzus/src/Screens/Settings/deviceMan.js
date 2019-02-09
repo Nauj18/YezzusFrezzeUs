dac3c5b189e978811871a8c45d081d39ff290822
davismitchell14 <dmitchell14@apu.edu>
2019-02-09 14:10:39 -0800

    Adds a button that prints success to console and takes you back to the setting screen. Removed the access code portion from the create a user screen

--- yezzus/src/Screens/Settings/deviceMan.js
+++ yezzus/src/Screens/Settings/deviceMan.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header, Button } from 'react-native-elements';
+import { Header, Button, Input } from 'react-native-elements';
+import { Spinner } from '../../components/common';
 
 class deviceMan extends Component {
+  applyPress = () => {
+    const { goBack } = this.props.navigation;
+    console.log('Success');
+    goBack();
+  };
+
+  renderButton() {
+    if (this.props.loading) {
+      return <Spinner size="large" />;
+    }
+
+    return (
+      <Button
+      title='Register Device'
+      titleStyle={{ fontSize: 20 }}
+      buttonStyle={{
+        width: 300,
+        height: 45
+      }}
+      containerStyle={{
+        alignItems: 'center',
+        marginTop: 15
+       }}
+       onPress={this.applyPress}
+      />
+    );
+  }
+
+
   render() {
     const { goBack } = this.props.navigation;
 
     return (
-      <View>
+      <View style={{ flex: 1 }}>
         <Header
           leftComponent={{
             icon: 'arrow-back',
@@ -15,7 +45,15 @@ class deviceMan extends Component {
             onPress: () => goBack() }}
           centerComponent={{ text: 'Device Manager', style: { color: '#fff' } }}
         />
-        <Text>This is the Device Manager Page!</Text>
+        <Input
+          label="Access Code"
+          placeholder="02937482"
+          onChange={this.handleChange}
+          value={this.props.device}
+          containerStyle={{ marginLeft: 10 }}
+        />
+
+        {this.renderButton()}
       </View>
     );
   }
