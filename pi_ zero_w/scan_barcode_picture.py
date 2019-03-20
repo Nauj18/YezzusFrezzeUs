5698ae940c925b1f43d466dffbb3158d4a659eca
jishii14 <jishii14@apu.edu>
2019-03-19 23:15:11 -0700

    Update scan_barcode_picture.py
    Added debug and also csv functionality to picture scanner just to get data sendable to react

--- pi_ zero_w/scan_barcode_picture.py
+++ pi_ zero_w/scan_barcode_picture.py
@@ -3,12 +3,16 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import argparse
 import time
+immport csv
 import cv2
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="path to input image")
 args = vars(ap.parse_args())
 image = cv2.imread(args["image"])
+
+csv = open(args["output"], "w")
+found = set()
 #camera = PiCamera()
 #rawCapture = PiRGBArray(camera)
 
@@ -19,6 +23,9 @@ image = cv2.imread(args["image"])
 #image = rawCapture.array
 barcodes = pyzbar.decode(image)
 
+# I added this to debug barcode images not working
+print(barcodes)
+
 for barcode in barcodes:
 	(x, y, w, h) = barcode.rect
 	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
@@ -28,6 +35,12 @@ for barcode in barcodes:
 
 	text = "{} ({})".format(barcodeData, barcodeType)
 	cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+	
+	# This is also in video script... we want to do this here too for now to get data in a way that is presentable to react
+	if barcodeData not in found:
+		csv.write("{}, {}\n".format(datetime.datetime.now(), barcodeData))
+		csv.flush()
+		found.add(barcodeData)
 
 	print("[INFO] Found {} barcode: {}".format(barcodeType, barcodeData))
 
