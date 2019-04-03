dcb44aa04394ca9d0d4e6058c41442f7bed66f12
Nauj18 <juanmm1818@gmail.com>
2019-04-02 20:19:27 -0700

    Post and get to firebase work for the pi. Have to work on extracting information from the json

--- pi_ zero_w/API_Connect.py
+++ pi_ zero_w/API_Connect.py
@@ -2,6 +2,7 @@ import json
 import requests
 from firebase import firebase
 import csv
+
 ################################################################################
 ## Will receive barcode and pass it though the open_food_facts api
 ## Prints json to terminal
@@ -10,41 +11,44 @@ import csv
 ##
 ## Example Barcode: 737628064502
 ##
+## Example UID: 03xQBYKBLFMhgEheyNTPFtV4LUi2
 ################################################################################
 
 baseUrl = "https://world.openfoodfacts.org/api/v0/product/"
 firebase = firebase.FirebaseApplication('https://yeesusfreezus.firebaseio.com/', None)
-# ref = db.reference('')
-
-result = firebase.get('/location/')
-
-
-print('Firebase stuff:' + result)
-
-print("\n")
-
 
 dates = []
 barcodes = []
 
-################################################################################
-## Read in barcode.csv file that stores the barcodes scanned from the camera
-##
-## Format: Date, Barcode
-################################################################################
+UP
 
-# with open('barcodes.csv') as csvfile:
-#     readCSV = csv.reader(csvfile, delimiter=',')
-#     for row in readCSV:
-#         dates.append(row[0])
-#         barcodes.append(row[1])
+def readCSVFile(dates,barcodes):
+    with open('barcodes.csv') as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        for row in readCSV:
+            if row == 1:
+                UID = row
+            else:
+                dates.append(row[0])
+                barcodes.append(row[1])
 
 
 #hardoded for testing, comment out later!
 barcode = "737628064502"
+uID = '03xQBYKBLFMhgEheyNTPFtV4LUi2'
 
 
-r = requests.get(url=baseUrl+'json?print=pretty')
+def getBarcodeInfo():
+    r = requests.get(url=baseUrl+barcode+'json?print=pretty')
+    productData = r.json()
 
 
-print(r.json())
+def getFirebase():
+    result = firebase.get('',None)
+    print (result)
+
+################################################################################
+## TODO: get Json Information and post it to firebase with the right format
+################################################################################
+def postFirebase():
+    firebase.put('',+uID+'Location/Freezer/'+dates,barcode)
